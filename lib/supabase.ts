03e7ebde21bf06ce7c4a494ce26f622e89a94a49
mahdysp <mahdyyyyy03@gmail.com>
2026-06-14 17:52:14 +0330

    fix

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = https://tceoklufqnoxmcxjndiv.supabase.co;
-const supabaseAnonKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjZW9rbHVmcW5veG1jeGpuZGl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE0MTY5MTcsImV4cCI6MjA5Njk5MjkxN30.b0HwecxxwhCkDiC51VlsPrw1gkDuG4daPuawsMhx4_Y;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
